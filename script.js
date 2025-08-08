3e7746fde4492eda2346cea5fd546e999936d535
ashish1987kumar <sharmashish2424@gmail.com>
2025-08-08 22:31:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element using its ID
+    const myButton = document.getElementById('myButton');
+
+    // Add a 'click' event listener to the button
+    myButton.addEventListener('click', function() {
+        // Display an alert message when the button is clicked
+        alert('Hello! The button was clicked.');
+    });
